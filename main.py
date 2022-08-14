cff07e8a26806c07c7564fcd4fb89af3e74f87b1
satwik-060 <perisetla.srisatwik00@gmail.com>
2022-08-14 19:10:40 +0530

    learned split and grabbing prices

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ with open('home.html', 'r') as html:
     content = html.read()
     
     soup = BeautifulSoup(content,'lxml')
-    tags = soup.find('h5') #search for first h5 tag
-    tags = soup.find_all('h5') #search for all h5 tag
-    for tag in tags :
-        print(tag.text)
-    # print(tags)
+    # tags = soup.find('h5') #search for first h5 tag
+    # tags = soup.find_all('h5') #search for all h5 tag
+    # for tag in tags :
+    #     print(tag.text)
+    # # print(tags)
+
+    course_cards = soup.find_all('div',class_='card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        print(f'course name : {course_name} and its price : {course_price}')
+
---
+++ split.py
@@ -0,0 +1,7 @@
+s1 = 'orange#hello#banana#bottle'
+s2 = "orange is a fruit"
+ans1 = s1.split('#')
+ans2 = s2.split()
+print(ans1)
+print(ans2)
+print(ans2[-1])
